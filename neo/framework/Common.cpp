85f3024ae5347fb86860978b58d3e27f44c7e1bb
Daniel Gibson <metalcaedes@gmail.com>
2025-10-27 05:17:08 +0100

    Make the Async Loop more precise
    by using Sys_MillisecondsPrecise() and Sys_SleepUntilPrecise()
    and by adding com_preciseFrameLengthMS (as float instead of int).
    
    Renamed com_gameFrameTime to com_gameFrameLengthMS, so it's less
    confusing with com_frameTime (which is the timestamp of the current
    frame, not its length)
    
    Based on the commit of the same name from the kickstart-my-hertz branch
    but removed some changes specific to configurable tickrate

--- neo/framework/Common.cpp
+++ neo/framework/Common.cpp
@@ -114,13 +114,21 @@ idCVar com_dbgServerAdr( "com_dbgServerAdr", "localhost", CVAR_SYSTEM | CVAR_ARC
 
 idCVar com_product_lang_ext( "com_product_lang_ext", "1", CVAR_INTEGER | CVAR_SYSTEM | CVAR_ARCHIVE, "Extension to use when creating language files." );
 
+// in the high-fps branch, the next three values will be set based on com_gameHz
+// here (in the old 60fps-only code) they're const and just to reduce difference to the other branch
+const int    com_gameHzVal = 60;
+const int    com_gameFrameLengthMS = 16; // length of one frame in msec, 1000 / com_gameHz
+const float  com_preciseFrameLengthMS = 16.6667f;    // 1000.0f / gameHzVal
+
+double com_preciseFrameTimeMS = 0; // like com_frameTime but as double: time (since start) for the current frame in milliseconds
+
 // com_speeds times
 int				time_gameFrame;
 int				time_gameDraw;
 int				time_frontend;			// renderSystem frontend time
 int				time_backend;			// renderSystem backend time
 
-int				com_frameTime;			// time (since start) for the current frame in milliseconds - TODO: DG: make it double?
+int				com_frameTime;			// time (since start) for the current frame in milliseconds
 int				com_frameNumber;		// variable frame number
 volatile int	com_ticNumber;			// 60 hz tics
 int				com_editors;			// currently opened editor(s)
@@ -265,7 +273,8 @@ void Com_UpdateFrameTime() {
 	int ticNum = com_ticNumber;
 	int ticDiff = ticNum - lastTicNum;
 	assert(ticDiff >= 0);
-	com_frameTime += ticDiff * USERCMD_MSEC;
+	com_preciseFrameTimeMS += ticDiff * com_preciseFrameLengthMS;
+	com_frameTime = idMath::Rint( com_preciseFrameTimeMS );
 	lastTicNum = ticNum;
 }
 
@@ -2550,8 +2559,8 @@ typedef struct {
 
 static const int MAX_ASYNC_STATS = 1024;
 asyncStats_t	com_asyncStats[MAX_ASYNC_STATS];		// indexed by com_ticNumber
-static int	lastTicMsec;
-static int	nextTicTargetMsec; // when (according to Sys_Milliseconds()) the next async tic should start
+static double lastTicMsec = 0.0;
+static double nextTicTargetMsec = 0.0; // when (according to Sys_Milliseconds()) the next async tic should start
 
 void idCommonLocal::SingleAsyncTic( void ) {
 	// main thread code can prevent this from happening while modifying
@@ -2594,19 +2603,20 @@ idCommonLocal::Async
 =================
 */
 void idCommonLocal::Async( void ) {
-	int	msec = Sys_Milliseconds();
+
+	double msec = Sys_MillisecondsPrecise();
 	if ( !lastTicMsec ) {
-		lastTicMsec = msec - USERCMD_MSEC;
+		lastTicMsec = msec - com_preciseFrameLengthMS;
 	}
 
 	if ( !com_preciseTic.GetBool() ) {
 		// just run a single tic, even if the exact msec isn't precise
 		SingleAsyncTic();
-		nextTicTargetMsec = msec + USERCMD_MSEC;
+		nextTicTargetMsec = msec + com_preciseFrameLengthMS;
 		return;
 	}
 
-	int ticMsec = USERCMD_MSEC;
+	float ticMsec = com_preciseFrameLengthMS;
 
 	// the number of msec per tic can be varies with the timescale cvar
 	float timescale = com_timescale.GetFloat();
@@ -2619,8 +2629,8 @@ void idCommonLocal::Async( void ) {
 
 	// don't skip too many
 	if ( timescale == 1.0f ) {
-		if ( lastTicMsec + 10 * USERCMD_MSEC < msec ) {
-			lastTicMsec = msec - 10*USERCMD_MSEC;
+		if ( lastTicMsec + 10 * com_preciseFrameLengthMS < msec ) {
+			lastTicMsec = msec - 10.0*com_preciseFrameLengthMS;
 		}
 	}
 
@@ -2845,14 +2855,22 @@ int idCommonLocal::AsyncThread(void* arg)
 
 	while ( self->runAsyncThread ) {
 
+		// The idea is to make this run super-exact, but round *down* com_gameFrameTime (USERCMD_MSEC).
+		// Then (I think..) when the game thread actually runs (0.x ms later than it might expect)
+		// all the things that waited for USERCMD_MSEC will run because they're (slightly) overdue
+		//  => they'll be exactly on time
+		// TODO: .. well, unless maybe if they waited for so many frametimes that we're a frame early..
+		//       But does it even matter for such long waits?
+		//       (and also, so far USERCMD_MSEC *has* been rounded down from 16.6667 to 16,
+		//        and with VSync enabled there was more or less correct timing)
+
 		self->Async();
 
+		// idSessionLocal::Frame() waits for TRIGGER_EVENT_ONE
+		// => this syncs the main thread with the async thread
 		Sys_TriggerEvent(TRIGGER_EVENT_ONE);
 
-		// TODO: -1 is so we don't sleep too long - would -2 be better, or can we have a more precise sleep?
-		//       IIRC especially on Windows sleeping is imprecise by at least on MS
-		int sleepTime = Max( 0, nextTicTargetMsec - (int)Sys_Milliseconds() - 1 );
-		Sys_Sleep( sleepTime );
+		Sys_SleepUntilPrecise( nextTicTargetMsec );
 	}
 	return 0;
 }
